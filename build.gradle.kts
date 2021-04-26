fa846ffc903c705681c5933b23b4d955dc1ffa2a
Brandon Fergerson <bfergerson@apache.org>
2021-04-26 12:11:20 -0400

    upgrade skywalking added postgres host auth md5 config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,3 @@
-import java.io.FileOutputStream
-import java.net.URL
-
 plugins {
     id("java")
     id("com.avast.gradle.docker-compose") version "0.14.3"
@@ -8,28 +5,6 @@ plugins {
 
 repositories {
     mavenCentral()
-}
-
-tasks {
-    register("downloadSkywalking") {
-        doLast {
-            println("Downloading Apache SkyWalking")
-            val f = File(projectDir, "docker/apache-skywalking-apm-es7-8.5.0.tar.gz")
-            if (!f.exists()) {
-                URL("https://downloads.apache.org/skywalking/8.5.0/apache-skywalking-apm-es7-8.5.0.tar.gz")
-                    .openStream().use { input ->
-                        FileOutputStream(f).use { output ->
-                            input.copyTo(output)
-                        }
-                    }
-            }
-            println("Downloaded Apache SkyWalking")
-        }
-    }
-
-    getByName("composeUp") {
-        dependsOn("downloadSkywalking")
-    }
 }
 
 dockerCompose {
