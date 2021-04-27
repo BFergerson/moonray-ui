3f8a3bbc3109541bc4bf4e4098eca41a429e48c8
Brandon Fergerson <bfergerson@apache.org>
2021-04-27 12:31:54 -0400

    still need dl

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import java.io.FileOutputStream
+import java.net.URL
+
 plugins {
     id("java")
     id("com.avast.gradle.docker-compose") version "0.14.3"
@@ -5,6 +8,28 @@ plugins {
 
 repositories {
     mavenCentral()
+}
+
+tasks {
+    register("downloadSkywalking") {
+        doLast {
+            println("Downloading Apache SkyWalking")
+            val f = File(projectDir, "docker/apache-skywalking-apm-es7-8.5.0.tar.gz")
+            if (!f.exists()) {
+                URL("https://dist.apache.org/repos/dist/dev/skywalking/8.5.0/apache-skywalking-apm-es7-8.5.0.tar.gz")
+                    .openStream().use { input ->
+                        FileOutputStream(f).use { output ->
+                            input.copyTo(output)
+                        }
+                    }
+            }
+            println("Downloaded Apache SkyWalking")
+        }
+    }
+
+    getByName("composeUp") {
+        dependsOn("downloadSkywalking")
+    }
 }
 
 dockerCompose {
